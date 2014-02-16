e4a65f3272898fd6a2aa08fd841f212ef4574b67
loranc <laurentcuzin@gmail.com>
2014-02-16 20:12:10 +0100

    Update 2_class_vs_instance.rb

--- 2-OO/2_class_vs_instance.rb
+++ 2-OO/2_class_vs_instance.rb
@@ -1,12 +1,21 @@
 #1. Dans chaque cas, dites s'il s'agit d'une méthode de classe ou d'instance !
+
 # un accesseur
+
 # le constructeur initialize
+class
 # la méthode new
+class
 # ActiveRecord#all
+class
 # ActiveRecord#find
+
 # CSV#open
+
 # Nokogiri::HTML#search
+
 # Faker::Name#name
+
 # Twitter::REST::Client#update
 
 
